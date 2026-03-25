e9dd4dddb59b5f43315a2c8a3e38ba232246b300
Alice Benatti <alice17bee@gmail.com>
2026-03-25 17:56:26 +0100

    fix: colors green-50 + other adm colors

--- pages/models/_id/index.vue
+++ pages/models/_id/index.vue
@@ -213,7 +213,11 @@
               :class="{ 'text-blue-700 hover:underline': isMeshFile(upload.filepath), 'text-gray-400 cursor-not-allowed': !isMeshFile(upload.filepath) }"
               :disabled="!isMeshFile(upload.filepath)"
               @click="openInCura(upload.filepath)"
-            ) Open in Cura
+            ) 
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M19 8H5c-1.66 0-3 1.34-3 3v4c0 1.1.9 2 2 2h2v2c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2v-2h2c1.1 0 2-.9 2-2v-4c0-1.66-1.34-3-3-3m-4 11H9c-.55 0-1-.45-1-1v-4h8v4c0 .55-.45 1-1 1m4-7c-.55 0-1-.45-1-1s.45-1 1-1s1 .45 1 1s-.45 1-1 1m-2-9H7c-.55 0-1 .45-1 1v2c0 .55.45 1 1 1h10c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1" />
+            </svg>
+              Open in Cura
             button.font-medium.text-black-700(type="button" class="hover:underline" @click="downloadUpload(upload.filepath)") Download
       .flex.justify-between.items-center.mb-6.w-full(v-if="boxFilesToDownload")
         <p class="w-full text-center bg-indigo-400 rounded-xl p-5 text-white font-semibold"><i>Hai trovato un modello che ti piace?</i> Scaricalo!<br>Caricalo su CURA e prepare il gcode da mettere su <a href="https://octoprint.org" target="_blank" class="text-white underline">Octoprint</a> per mandarlo in stampa.</p>
